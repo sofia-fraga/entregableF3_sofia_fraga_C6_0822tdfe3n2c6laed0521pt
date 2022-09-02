10a26bbc06cde6aeac9e0d6eebe7b7a56e1a9214
sofia fraga <iq.sofiafraga@gmail.com>
2022-09-01 22:39:48 -0300

    se quitan comentarios y espacios innecesarios en el código

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-// El componente App es el padre de:
-// - Cabecera
-// - Listado
-// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
-// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
-// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
-
 import {useState} from 'react';
 import Cabecera from './componentes/Cabecera';
 import Listado from './componentes/Listado';
 import data from './componentes/data.json';
 
 function App() {
-
     const [totalElementos, setTotalElementos] = useState(0);
-
+    
     const incrementarTotal = () =>{
-        setTotalElementos(totalElementos+1)
+      setTotalElementos(totalElementos+1)
     }
 
     return (
       <div className="App">
-        <Cabecera carrito={totalElementos} />
+        <Cabecera carrito={totalElementos}/>
         <Listado data={data} incrementarTotal={incrementarTotal}/>
       </div>
     );
   }
   
-  export default App;
+export default App;
--- src/componentes/Cabecera.js
+++ src/componentes/Cabecera.js
@@ -1,18 +1,10 @@
-// El componente Cabera no tiene componentes hijos.
-// ESTADO: Cabecera no tiene estado.
-// MÉTODOS: Cabecera no tiene métodos.
-// PROPS: Cabecera recibe de su padre la cantidad que va a mostrar dentro del span correpondiente
-// Maqueta de Cabecera:
-//    h1
-//    p > span     (el span mostrará la cantidad recibida por props)
 import React from 'react'
 
 export default function Cabecera({carrito}) {
-  
   return (
     <header>
-        <h1> Carrito de compras </h1>
-        <p> Cantidad de productos <span>{carrito}</span> </p>
+      <h1> Carrito de compras </h1>
+       <p> Cantidad de productos <span>{carrito}</span> </p>
     </header>
   )
 }
